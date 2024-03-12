0acd944e5d8a615fbb89b50ba095b9ad32d8f9b0
Ritesh Kumar <98487547+thebrandrk@users.noreply.github.com>
2024-03-12 13:07:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    centeredSlides: true,
+    spaceBetween: 30,
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+  });
